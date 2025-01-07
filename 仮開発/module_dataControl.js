532599d47d0e94605da887c168d1159257b36a24
pikupiku-official <pikupiku.official@gmail.com>
2025-01-07 18:06:45 +0900

    セーブシステムの修正

--- 仮開発/module_dataControl.js
+++ 仮開発/module_dataControl.js
@@ -4,9 +4,10 @@ function SaveGame() {
     // データをlocalStorageに保存
     localStorage.setItem('stepCounter', stepCounter); // キーを文字列として指定
     localStorage.setItem('stageNumber', stageNumber); // キーを文字列として指定
-    localStorage.setItem('gPlayerX', gPlayerX);
-    localStorage.setItem('gPlayerY', gPlayerY);
-    localStorage.setItem('gAngle', gAngle);
+    // localStorage.setItem('gPlayerX', gPlayerX);
+    // localStorage.setItem('gPlayerY', gPlayerY);
+    // localStorage.setItem('gAngle', gAngle);
+    // localStorage.setItem('boxes', boxes);
     console.log("The data saved. step: " + localStorage.getItem('stepCounter') + " stage: " + localStorage.getItem('stageNumber') + 
     " player: " + localStorage.getItem('gPlayerX') + ", " + localStorage.getItem('gPlayerY'));
 }
@@ -15,9 +16,11 @@ function LoadGame() {
     // データをlocalStorageから読み込み
     stepCounter = parseInt(localStorage.getItem('stepCounter')) || 0; // データがnullの場合はデフォルト値を設定
     stageNumber = parseInt(localStorage.getItem('stageNumber')) || 0; // データがnullの場合はデフォルト値を設定
-    gPlayerX = parseInt(localStorage.getItem('gPlayerX')) || 48;
-    gPlayerY = parseInt(localStorage.getItem('gPlayerY')) || 112;
-    gAngle = parseInt(localStorage.getItem('gAngle')) || 0;
+    // gPlayerX = parseInt(localStorage.getItem('gPlayerX')) || 48;
+    // gPlayerY = parseInt(localStorage.getItem('gPlayerY')) || 112;
+    // gAngle = parseInt(localStorage.getItem('gAngle')) || 0;
+    // boxes = parseInt(localStorage.getItem('boxes')) || 0;
     console.log("The data loaded. step: " + stepCounter + " stage: " + stageNumber + " player: " + gPlayerX + ", " + gPlayerY);
     LoadData();
+    InitializeEvent();
 }
